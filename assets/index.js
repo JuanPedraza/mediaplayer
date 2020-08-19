67d75649c38933d6be150d2a3ee8df39894bfe02
Juan Pedraza <juan_pedraza@outlook.com>
2020-08-19 00:05:59 -0500

    css edit and module too

--- assets/index.js
+++ assets/index.js
@@ -1,19 +1,9 @@
+import mediaPlayer from './mediaplayer.js';
+
 const video = document.querySelector("video");
 const button = document.querySelector("button");
 
-function mediaPlayer(config) {
-    this.media = config.el;
-}
 
-mediaPlayer.prototype.play = function () {
-    if (this.media.paused) {
-    this.media.play();
-    } else {
-    this.media.pause();
-    }
-
-  // this.media.paused ? this.media.play() : this.media.pause()
-};
 
 const player = new mediaPlayer({ el: video });
 button.onclick = () => player.play();
---
+++ assets/mediaplayer.js
@@ -0,0 +1,19 @@
+
+
+function mediaPlayer(config) {
+    this.media = config.el;
+}
+
+mediaPlayer.prototype.play = function () {
+    if (this.media.paused) {
+        this.media.play();
+    } else {
+        this.media.pause();
+    }
+    
+    // this.media.paused ? this.media.play() : this.media.pause()
+};
+
+// export default mediaPlayer
+
+export default mediaPlayer
