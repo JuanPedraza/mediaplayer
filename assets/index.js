e41102938df7dd067a0ca3059bffafb05958523c
Juan Pedraza <juan_pedraza@outlook.com>
2020-08-19 21:55:04 -0500

    adding plugins

--- assets/index.js
+++ assets/index.js
@@ -1,9 +1,26 @@
 import mediaPlayer from './mediaplayer.js';
+import autoPlay from './plugins/autoPlay.js'
+
 
 const video = document.querySelector("video");
-const button = document.querySelector("button");
+// const button = document.querySelector("button");
+const player = new mediaPlayer({ el: video, plugins: [
+    new autoPlay()
+] });
+
+const playButton = document.querySelector('#playButton')
+playButton.onclick = () => player.play();
+
+const muteButton = document.querySelector('#muteButton')
+muteButton.onclick = () => {
+    if(player.media.muted){
+        player.unmute()
+    }else {
+        player.mute()
+    }
+
+    
+}
 
 
 
-const player = new mediaPlayer({ el: video });
-button.onclick = () => player.play();
--- assets/mediaplayer.js
+++ assets/mediaplayer.js
@@ -2,6 +2,25 @@
 
 function mediaPlayer(config) {
     this.media = config.el;
+    this.plugins = config.plugins || []
+
+
+    this._initPlugins()
+}
+
+
+mediaPlayer.prototype._initPlugins = function(){
+    this.plugins.forEach(plugin => {
+        plugin.run(this)
+    });
+}
+
+mediaPlayer.prototype.mute = function () {
+    this.media.muted = true
+}
+
+mediaPlayer.prototype.unmute = function () {
+    this.media.unmute = false
 }
 
 mediaPlayer.prototype.play = function () {
